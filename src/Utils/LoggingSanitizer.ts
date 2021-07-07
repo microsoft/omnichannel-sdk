301510047256c41c9f0ffc9bc83f958bc96dfde6
MURTAZA MANASAWALA <mumanasa@microsoft.com>
2021-07-07 14:55:17 -0700

    Removing unwanted space

--- src/Utils/LoggingSanitizer.ts
+++ src/Utils/LoggingSanitizer.ts
@@ -3,7 +3,7 @@ import Constants from "../Common/Constants";
 export class LoggingSanitizer  {
   public static stripErrorSensitiveProperties(errorObject: any): void { // eslint-disable-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
     if(errorObject && typeof errorObject === 'object' && Object.keys(errorObject)?.length > 0) {
-      Object.keys(errorObject)?.forEach( (key) => {
+      Object.keys(errorObject)?.forEach((key) => {
           if (Constants.sensitiveProperties.indexOf(key) !== -1) {
             // remove sensitive properties from error object
             delete errorObject[`${key}`];
