c2bca35e3321c71df4ff2779f9be0cef3e4434ab
Edgar Lopez <elopezanaya@gmail.com>
2023-01-28 20:16:30 -0800

    new constant

--- src/Common/Constants.ts
+++ src/Common/Constants.ts
@@ -13,7 +13,9 @@ export default class Constants {
   public static readonly defaultLocale = "en-us";
   public static readonly noContentStatusCode = 204;
   public static readonly tooManyRequestsStatusCode = 429;
-  public static readonly sensitiveProperties = ["AuthenticatedUserToken"];
+  public static readonly outOfOfficeErrorCode = 705;
+  
+public static readonly sensitiveProperties = ["AuthenticatedUserToken"];
   public static readonly transactionid = "transaction-id";
   public static readonly customerDisplayName = "customerDisplayName";
   public static readonly hiddenContentPlaceholder = "*content hidden*";
