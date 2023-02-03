39477fc93691e3bd22e8697f189f6bb57e21112d
Edgar Lopez <elopezanaya@gmail.com>
2023-02-02 16:50:15 -0800

    format

--- src/Utils/LoggingSanitizer.ts
+++ src/Utils/LoggingSanitizer.ts
@@ -33,42 +33,43 @@ export class LoggingSanitizer {
     }
   }
 
-  public static stripErrorSensitiveProperties(errorObject: any): void { // eslint-disable-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types     
-    if (errorObject && typeof errorObject === 'object' && Object.keys(errorObject)?.length > 0) {
+  public static stripErrorSensitiveProperties(errorObject: any): void { // eslint-disable-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+    if(errorObject && typeof errorObject === 'object' && Object.keys(errorObject)?.length > 0) {
       Object.keys(errorObject)?.forEach((key) => {
-        if (Constants.sensitiveProperties.indexOf(key) !== -1) {
-          // remove sensitive properties from error object
-          delete errorObject[`${key}`];
-        }
-        if (key === 'data') {
-          let data;
-
-          if (typeof errorObject[key] === 'string') { // eslint-disable-line security/detect-object-injection
-            try {
-              data = JSON.parse(errorObject[key]); // eslint-disable-line security/detect-object-injection
-            } catch {
-              data = undefined;
-            }
-          
+          if (Constants.sensitiveProperties.indexOf(key) !== -1) {
+            // remove sensitive properties from error object
+            delete errorObject[`${key}`];
           }
-          if (data) {
-            if (Object.keys(data).includes('preChatResponse')) {
-              LoggingSanitizer.stripPreChatResponse(data.preChatResponse);
+
+          if (key === 'data') {
+            let data;
+            if (typeof errorObject[key] === 'string') { // eslint-disable-line security/detect-object-injection
+              try {
+                data = JSON.parse(errorObject[key]); // eslint-disable-line security/detect-object-injection
+              } catch {
+                data = undefined;
+              }
             }
 
-            if (Object.keys(data).includes('customContextData')) {
-              LoggingSanitizer.stripCustomContextDataValues(data.customContextData);
-            }
+            if (data) {
+              if (Object.keys(data).includes('preChatResponse')) {
+                LoggingSanitizer.stripPreChatResponse(data.preChatResponse);
+              }
 
-            LoggingSanitizer.stripGeolocation(data);
-            errorObject[key] = JSON.stringify(data); // eslint-disable-line security/detect-object-injection
+              if (Object.keys(data).includes('customContextData')) {
+                LoggingSanitizer.stripCustomContextDataValues(data.customContextData);
+              }
+
+              LoggingSanitizer.stripGeolocation(data);
+              errorObject[key] = JSON.stringify(data); // eslint-disable-line security/detect-object-injection
+            }
           }
-        }
-        if (errorObject[`${key}`] !== null && typeof errorObject[`${key}`] === 'object') {
-          // check sensitive properties in nested error object
-          this.stripErrorSensitiveProperties(errorObject[`${key}`]);
-          return;
-        }
+
+          if (errorObject[`${key}`] !== null && typeof errorObject[`${key}`] === 'object') {
+            // check sensitive properties in nested error object
+            this.stripErrorSensitiveProperties(errorObject[`${key}`]);
+            return;
+          }
       });
     }
   }
