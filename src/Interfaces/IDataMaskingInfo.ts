b1a0d6c111bb0ae2b11abc05fd06b0783626e045
Caleb Niemann <canieman@microsoft.com>
2025-11-24 14:35:38 -0700

    Removing stale change

--- src/Interfaces/IDataMaskingInfo.ts
+++ src/Interfaces/IDataMaskingInfo.ts
@@ -7,7 +7,7 @@ interface IDataMaskingSetting {
   maskForCustomer: boolean;
   maskForAgent: boolean;
 }
-// Adding a comment for a change
+
 export default interface IDataMaskingInfo {
   dataMaskingRules: IDataMaskingRule[];
   setting: IDataMaskingSetting;
